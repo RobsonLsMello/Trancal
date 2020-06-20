c7c831c71f37f540ae363e7efe8722e3bcd05ca9
Robson Lucas Silveira Mello <robson.mello3@etec.sp.gov.br>
2020-06-20 00:10:33 -0300

    1.3.0
    Adicionado o tutorial

---
+++ script/tutorial.js
@@ -0,0 +1,25 @@
+document.querySelector("#pular").addEventListener("click", ()=>{
+    document.querySelector("#tutorial").classList.add("hidden");
+})
+
+document.querySelector("#ajuda").addEventListener("click", ()=>{
+    document.querySelector("#tutorial").classList.remove("hidden");
+})
+
+document.querySelector("#iniciarTutorial").addEventListener("click", ()=>{
+    document.querySelector("#tutorial").classList.add("hidden");
+    document.querySelector("#primeiro").classList.remove("hidden");
+})
+
+document.querySelector("#proximo1").addEventListener("click", ()=>{
+    document.querySelector("#primeiro").classList.add("hidden");
+    document.querySelector("#segundo").classList.remove("hidden");
+})
+
+document.querySelector("#proximo2").addEventListener("click", ()=>{
+    document.querySelector("#segundo").classList.add("hidden");
+    document.querySelector("#terceiro").classList.remove("hidden");
+})
+document.querySelector("#finalizar").addEventListener("click", ()=>{
+    document.querySelector("#terceiro").classList.add("hidden");
+})
